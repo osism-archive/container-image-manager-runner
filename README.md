bd352b363796e9a2480b41a6c349d8fe94af93a8
Christian Berendt <berendt@betacloud-solutions.de>
2021-04-10 16:59:22 +0200

    Initial commit
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

---
+++ README.md
@@ -0,0 +1,3 @@
+# container-image-manager-runner
+
+[![Quay](https://img.shields.io/badge/Quay-osism%2Fmanager--runner-blue.svg)](https://quay.io/repository/osism/manager-runner)
---
+++ scripts/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -x
+
+# Available environment variables
+#
+# BUILD_OPTS
+# DOCKER_REGISTRY
+# REPOSITORY
+# VERSION
+
+# Set default values
+
+BUILD_OPTS=${BUILD_OPTS:-}
+CREATED=$(date --rfc-3339=ns)
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
+REPOSITORY=${REPOSITORY:-osism/manager-runner}
+REVISION=$(git rev-parse --short HEAD)
+VERSION=${VERSION:-latest}
+
+if [[ -n $DOCKER_REGISTRY ]]; then
+    REPOSITORY="$DOCKER_REGISTRY/$REPOSITORY"
+fi
+
+docker buildx build \
+    --load \
+    --build-arg "VERSION=$VERSION" \
+    --tag "$REPOSITORY:$VERSION" \
+    --label "org.opencontainers.image.created=$CREATED" \
+    --label "org.opencontainers.image.documentation=https://docs.osism.de" \
+    --label "org.opencontainers.image.licenses=ASL 2.0" \
+    --label "org.opencontainers.image.revision=$REVISION" \
+    --label "org.opencontainers.image.source=https://github.com/osism/docker-image-manager" \
+    --label "org.opencontainers.image.title=manager" \
+    --label "org.opencontainers.image.url=https://www.osism.de" \
+    --label "org.opencontainers.image.vendor=Betacloud Solutions GmbH" \
+    --label "org.opencontainers.image.version=$VERSION" \
+    --file Dockerfile $BUILD_OPTS .
